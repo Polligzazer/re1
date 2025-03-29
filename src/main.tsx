0afc33be9347542d2aa4aae03e1c07fa646af4ec
Polligzazer <leikezea2007@gmail.com>
2025-03-29 20:56:20 +0800

    Test

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import {AuthContextProvider} from "../components/Authcontext"
-import {ChatContextProvider} from "../components/ChatContext"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { ChatContextProvider } from "../components/ChatContext";
+import { AuthContextProvider } from "../components/Authcontext";
 
 createRoot(document.getElementById('root')!).render(
-    <AuthContextProvider>
-      <ChatContextProvider>
-        <StrictMode>
-          <App />
-        </StrictMode>
-      </ChatContextProvider>
-    </AuthContextProvider>
-)
+  <AuthContextProvider>
+  <ChatContextProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </ChatContextProvider>
+  </AuthContextProvider>
+);
--- vite.config.ts
+++ vite.config.ts
@@ -4,20 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  envPrefix: 'VITE_',
-  server: {
-    host: true,
-    proxy: {
-      "/v1": {
-        target: "https://cloud.appwrite.io",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
     rollupOptions: {
       external: ["font-awesome"]
     }
-  }
+  },
+
 })
